81b71e96c5ac06a5d86a94dcb74739c9104a46c9
Andy Bennett <andyjpb@digital.cabinet-office.gov.uk>
2019-03-19 18:46:06 +0000

    slang.py: Initial parser framework for the spreadsheet metadata language
    We can parse the `declare-type`, `declare-header`, `declare-data` verbs as
    specified in the proof-of-concept proposal at
    
    https://docs.google.com/presentation/d/1eEc8s3_eNx_b5vMxWpF-BiueDfaz6mD6zbamXbSy1Z4
    
    We also have a rudimentary comment facility through the `#` verb.
    
    poc.slang contains the example metadata from the proof-of-concept proposal.
    
    -----
    $ make run
    python
    Python 2.7.10 (default, Oct  6 2017, 22:29:07)
    [GCC 4.2.1 Compatible Apple LLVM 9.0.0 (clang-900.0.31)] on darwin
    Type "help", "copyright", "credits" or "license" for more information.
    >>> import slang
    >>> x = slang.slang(open("poc.slang"))
    >>> x.parse()
    -----
    
    Signed-off-by: Andy Bennett <andyjpb@digital.cabinet-office.gov.uk>

---
+++ slang.py
@@ -0,0 +1,156 @@
+###############################################################################
+###
+### slang.py - An implementation of the Spreadsheet Description Language.
+###
+###  gov.uk Metadata Standards are a way to increase the interoperability of
+###  spreadsheets between government departments.
+###
+###  slang.py implements the spreadsheet description language described in
+###  https://docs.google.com/presentation/d/1eEc8s3_eNx_b5vMxWpF-BiueDfaz6mD6zbamXbSy1Z4
+###
+###
+###  Copyright (C) 2019, Andy Bennett, Crown Copyright (Government Digital Service).
+###
+###  Permission is hereby granted, free of charge, to any person obtaining a
+###  copy of this software and associated documentation files (the "Software"),
+###  to deal in the Software without restriction, including without limitation
+###  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+###  and#or sell copies of the Software, and to permit persons to whom the
+###  Software is furnished to do so, subject to the following conditions:
+###
+###  The above copyright notice and this permission notice shall be included in
+###  all copies or substantial portions of the Software.
+###
+###  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+###  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+###  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+###  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+###  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+###  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+###  DEALINGS IN THE SOFTWARE.
+###
+### Andy Bennett <andyjpb@digital.cabinet-office.gov.uk>, 2019/03/19
+###
+###############################################################################
+
+# language.py cannot be invoked directly. You should import it into another
+# program thus:
+# import slang
+
+import sys
+
+
+
+###############################################################################
+# Internal Representation of a Spreadsheet Metadata Language description
+
+class state:
+
+    def __init__(self):
+        True
+
+
+    # declare-type Price GBPxVAT
+    def declare_type(self, name, type):
+
+        assert isinstance(name, str), ("state.declare_type: Expected name argument to be of type 'str' but e got %s." % name)
+        assert isinstance(type, str), ("state.declare_type: Expected type argument to be of type 'str' but e got %s." % type)
+
+        print("declare_type: name = %s, type = %s" % (name, type))
+
+
+    # declare-header A3:D3
+    def declare_header(self, range):
+
+        assert isinstance(range, str), ("state.declare_header: Expected range argument to be of type 'str' but e got %s." % range)
+
+        print("declare_header: range = %s" % (range))
+
+
+    # declare-data A4:D8
+    def declare_data(self, range):
+
+        assert isinstance(range, str), ("state.declare_data: Expected range argument to be of type 'str' but e got %s." % range)
+
+        print("declare_data: range = %s" % (range))
+
+
+    # Internal state
+    keys   = {}
+    header = None
+    data   = None
+
+
+
+###############################################################################
+# Handlers for each verb in the Spreadsheet Metadata Language
+
+# Handler for comments in metadata files.
+# Ignore them!
+def comment(*args):
+    None
+
+# A list mapping verbs to procedures that expect a state object and the
+# arguments specified.
+handlers = {
+        "declare-type"   : state.declare_type,
+        "declare-header" : state.declare_header,
+        "declare-data"   : state.declare_data,
+        "#"              : comment,
+        }
+
+
+
+###############################################################################
+# ADT for the Spreadsheet Metadata Language
+
+class slang:
+
+    # input is a file descriptor for the metadata
+    def __init__(self, input = sys.stdin):
+
+        assert isinstance(input, file), ("slang.__init__: Expected input argument to be of type 'file' but we got %s." % input)
+
+        self.input = input
+        self.state = None
+
+
+    # Parses the metadata file that we've been given and internalises it into
+    # the state object. This procedure can only be called once per slang
+    # object.
+    def parse(self):
+
+        assert (self.state == None), ("slang.parse; parsing has already been done for this object!")
+
+        self.state = state()
+
+        line = self.input.readline()
+        line_no = 1
+
+        while line != "":
+            line = line.rstrip('\n')
+            original = line
+            line = line.split("\t")
+            if line == ['']:
+                # Ignore blank lines
+                line = self.input.readline()
+                line_no += 1
+                continue
+
+            proc      = line[0]
+            arguments = line[1:]
+            arguments.reverse()
+            arguments.append(self.state)
+            arguments.reverse()
+
+            assert (proc in handlers), ("slang.parse: We don't know how to handle \"%s\" on line %s." % (original, line_no))
+
+            try:
+                apply(handlers[proc], arguments)
+            except:
+                print("\n\nslang.parse: Error while handling \"%s\" on line %s.\n" % (original, line_no))
+                raise
+
+            line = self.input.readline()
+            line_no += 1
+
