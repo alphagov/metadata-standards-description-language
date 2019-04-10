bf042a84991fff20e79e7b6e1cc1226b4f1ff96f
Andy Bennett <andyjpb@digital.cabinet-office.gov.uk>
2019-04-10 17:39:29 +0100

    Mockup from the vision spreadsheet
    This a mockup of the spreadsheet refered to in the original
    proof-of-concept proposal at the following URL.
    
    https://docs.google.com/presentation/d/1eEc8s3_eNx_b5vMxWpF-BiueDfaz6mD6zbamXbSy1Z4
    
    Signed-off-by: Andy Bennett <andyjpb@digital.cabinet-office.gov.uk>

--- widgets/render.py
+++ widgets/render.py
@@ -19,9 +19,33 @@ def contents_only(widget):
 
 
 page                 = load_widget("page.html")
+two_thirds_one_third = load_widget("two-thirds-one-third.html")
+button               = contents_only(load_widget("button.html"))
+spreadsheet          = load_widget("spreadsheet.html")
+heading_l            = contents_only(load_widget("heading-l.html"))
+heading_s            = contents_only(load_widget("heading-s.html"))
+radio_buttons        = contents_only(load_widget("radio-buttons.html"))
+radio_item           = contents_only(load_widget("radio-item.html"))
 
 
 print \
 page({"title"   : "Submit Your Spreadsheet To Treasury",
+    "contents": two_thirds_one_third ({
+        "heading"       : "Submit Your Spreadsheet To Treasury",
+        "first_column"  : cat(
+            heading_s("Select the area that contains your spending data"),
+            spreadsheet({}),
+            button("Save and continue")),
+        "second_column" : cat(
+            radio_buttons(cat(
+                radio_item("Number"),
+                radio_item("Number Of Laptops"),
+                radio_item("Currency"),
+                radio_item("GBP ex. VAT"),
+                radio_item("GBP inc. VAT"),
+                radio_item("Product Code"),
+                radio_item("Text")
+                )))
+        })
     })
 
